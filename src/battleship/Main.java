36a5c0d6ca5c4195a0aa4190506466d6cda9df37
Denis Stoyanouski <vandenst@tut.by>
2022-06-23 18:07:02 +0300

    Added replacement of ship on the field with checking 1 Wrong length of the Submarine! 2 Wrong ship location 3 It too close to another one

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -10,12 +10,18 @@ public class Main {
     static String coordinateBegin;
     static String coordinateEnd;
 
+    static int beginVer;
+    static int beginHor;
+    static int endVer;
+    static int endHor;
+
     final private static List<String> verAddress = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J");
 
     final private static List<String> horAddress = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
 
     public static void main(String[] args) {
         Field();
+        System.out.println("Enter the coordinates of the Aircraft Carrier (5 cells):");
         Coordinate();
         CurrentField();
     }
@@ -33,21 +39,28 @@ public class Main {
 
     public static void Coordinate() {
 
-        System.out.println("Enter the coordinates of the Aircraft Carrier (5 cells):");
         try (Scanner scanner = new Scanner(System.in)) {
             boolean check;
             do {
                 check = true;
                 coordinateBegin = scanner.next().toUpperCase();
                 coordinateEnd = scanner.next().toUpperCase();
+                //return input data into useful form for array;
+                beginVer = verAddress.indexOf(coordinateBegin.substring(0, 1));
+                beginHor = horAddress.indexOf(coordinateBegin.substring(1));
+                endVer = verAddress.indexOf(coordinateEnd.substring(0, 1));
+                endHor = horAddress.indexOf(coordinateEnd.substring(1));
+
                 if (!coordinateBegin.matches("[A-Z]\\d\\d?") || !coordinateEnd.matches("[A-Z]\\d\\d?")) {
                     System.out.println("Try again");
                     check = false;
                 } else if (!verAddress.contains(coordinateBegin.substring(0, 1)) || !horAddress.contains(coordinateBegin.substring(1))) {
                     System.out.println("Change the coordinate of the begin");
                     check = false;
-                } else if (!verAddress.contains(coordinateEnd.substring(0,1)) || !horAddress.contains(coordinateEnd.substring(1))) {
+                } else if (!verAddress.contains(coordinateEnd.substring(0, 1)) || !horAddress.contains(coordinateEnd.substring(1))) {
                     System.out.println("Change the coordinate of the end");
+                    check = false;
+                } else if (!checkReplacementOfShip()) {
                     check = false;
                 }
             } while (!check);
@@ -56,52 +69,50 @@ public class Main {
             e.getMessage();
         }
 
+        ReplacementOfShip();
+    }
 
-        //return input data into useful form for array;
-        int beginVer = verAddress.indexOf(String.valueOf(coordinateBegin.charAt(0)));
-        int beginHor = horAddress.indexOf(String.valueOf(coordinateBegin.charAt(1)));
-        int endVer = verAddress.indexOf(String.valueOf(coordinateEnd.charAt(0)));
-        int endHor = horAddress.indexOf(String.valueOf(coordinateEnd.charAt(1)));
-        battleField[0][0] = '0';
+    static boolean checkReplacementOfShip() {
+        boolean checkReplacement = true;
 
-        boolean fuckCheck = true;
-            //check size of ship
+        //check size of ship
         if (Math.abs(beginHor - endHor) != 4 && Math.abs(beginVer - endVer) != 4) {
             System.out.println("Error! Wrong length of the Submarine! Try again:");
-            fuckCheck = false;
-        }
-        //check ship direction is not diagonal;
-        if (beginHor != endHor && beginVer != endVer) {
+            checkReplacement = false;
+        } else if (beginHor != endHor && beginVer != endVer) { //check ship direction is not diagonal;
             System.out.println("Error! Wrong ship location! Try again:");
-            fuckCheck = false;
-        }
-        //check placing to another one;
-        try {
+            checkReplacement = false;
+        } else { //check placing to another one;
             for (int i = beginHor - 1; i <= endHor + 1; i++) {
-                for (int j = beginVer - 1; j <= endVer + 1; j++) {
-                    if (battleField[j][i] == '0') {
-                        fuckCheck = false;
-                        System.out.println("Error! You placed it too close to another one. Try again:");
-                        break;
+                if (i > 0 && i < 10) {
+                    for (int j = beginVer - 1; j <= endVer + 1; j++) {
+                        if (j > 0 && j < 10) {
+                            if (battleField[j][i] == '0') {
+                                System.out.println("Error! You placed it too close to another one. Try again:");
+                                checkReplacement = false;
+                                break;
+                            }
+                        }
                     }
                 }
             }
-        } catch (Exception e) {
-            // just do nothing
         }
-        if (fuckCheck) {
-            // filling cells with 0;
-            if (beginHor == endHor) {
-                for (int i = beginVer; i <= endVer; i++) {
-                    battleField[i][endHor] = 'O';
-                }
-            } else {
-                for (int i = beginHor; i<= endHor; i++) {
-                    battleField[beginVer][i] = 'O';
-                }
+        return checkReplacement;
+    }
+
+    static void ReplacementOfShip() {
+        if (beginHor == endHor) {
+            for (int i = beginVer; i <= endVer; i++) {
+                battleField[i][endHor] = 'O';
+            }
+        } else {
+            for (int i = beginHor; i <= endHor; i++) {
+                battleField[beginVer][i] = 'O';
             }
         }
     }
+
+
 
     public static void CurrentField() {
         // display current version of field;
