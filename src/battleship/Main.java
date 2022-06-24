18240a333fec0be13245237527717e7f1a8f4a65
Denis Stoyanouski <vandenst@tut.by>
2022-06-24 23:29:13 +0300

    Added objectives

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -2,6 +2,23 @@ package battleship;
 
 import java.util.*;
 
+/*
+Objectives
+        In this stage, you should arrange your ships on the game field. Before you start, let's discuss the conventions
+         of the game:
+
+        * On a 10x10 field, the first row should contain numbers from 1 to 10 indicating the column, and the first column
+        should contain letters from A to J indicating the row.
+        * The symbol ~ denotes the fog of war: the unknown area on the opponent's field and the yet untouched area on
+        your field.
+        * The symbol O denotes a cell with your ship, X denotes that the ship was hit, and M signifies a miss.
+        * You have 5 ships: Aircraft Carrier is 5 cells, Battleship is 4 cells, Submarine is 3 cells, Cruiser is also
+        3 cells, and Destroyer is 2 cells. Start placing your ships with the largest one.
+        * To place a ship, enter two coordinates: the beginning and the end of the ship.
+        * If an error occurs in the input coordinates, your program should report it. The message should contain the word
+        Error.
+*/
+
 
 public class Main {
 
@@ -33,8 +50,8 @@ public class Main {
     }
 
     public static void Coordinate() {
-        String coordinateBegin;
-        String coordinateEnd;
+        String inputFirst;
+        String inputSecond;
         int beginVer;
         int beginHor;
         int endVer;
@@ -44,15 +61,22 @@ public class Main {
                 for (String ship : typeOfShip) {
                     int length = lengthOfShip.get(typeOfShip.indexOf(ship));
                     System.out.printf("Enter the coordinates of the %s (%d cells):%n", ship, length);
-                    do {
-                        coordinateBegin = scanner.next().toUpperCase();
-                        coordinateEnd = scanner.next().toUpperCase();
-                        //return input data into useful form for array;
-                        beginVer = verAddress.indexOf(coordinateBegin.substring(0, 1));
-                        beginHor = horAddress.indexOf(coordinateBegin.substring(1));
-                        endVer = verAddress.indexOf(coordinateEnd.substring(0, 1));
-                        endHor = horAddress.indexOf(coordinateEnd.substring(1));
-                    } while (!checkOfInput(coordinateBegin, coordinateEnd, length, beginHor, beginVer, endHor, endVer));
+                    do { //return input data into useful form for array;
+                        inputFirst = scanner.next().toUpperCase();
+                        inputSecond = scanner.next().toUpperCase();
+                        if (inputFirst.compareTo(inputSecond) > 0 || inputFirst.length() > inputSecond.length()) {
+                            endVer = verAddress.indexOf(inputFirst.substring(0, 1));
+                            endHor = horAddress.indexOf(inputFirst.substring(1));
+                            beginVer = verAddress.indexOf(inputSecond.substring(0, 1));
+                            beginHor = horAddress.indexOf(inputSecond.substring(1));
+
+                        } else {
+                            beginVer = verAddress.indexOf(inputFirst.substring(0, 1));
+                            beginHor = horAddress.indexOf(inputFirst.substring(1));
+                            endVer = verAddress.indexOf(inputSecond.substring(0, 1));
+                            endHor = horAddress.indexOf(inputSecond.substring(1));
+                        }
+                    } while (!checkOfInput(inputFirst, inputSecond, length, beginHor, beginVer, endHor, endVer));
                     ReplacementOfShip(beginHor, beginVer, endHor, endVer);
                 }
             } catch(Exception e){
@@ -61,15 +85,15 @@ public class Main {
 
     }
 
-    static boolean checkOfInput(String coordinateEnd, String coordinateBegin, int length, int beginHor, int beginVer, int endHor, int endVer) {
+    static boolean checkOfInput(String inputFirst, String inputSecond, int length, int beginHor, int beginVer, int endHor, int endVer) {
         boolean check = true;
-        if (!coordinateBegin.matches("[A-Z]\\d\\d?") || !coordinateEnd.matches("[A-Z]\\d\\d?")) {
+        if (!inputFirst.matches("[A-Z]\\d\\d?") || !inputSecond.matches("[A-Z]\\d\\d?")) {
             System.out.println("Try again");
             check = false;
-        } else if (!verAddress.contains(coordinateBegin.substring(0, 1)) || !horAddress.contains(coordinateBegin.substring(1))) {
+        } else if (!verAddress.contains(inputFirst.substring(0, 1)) || !horAddress.contains(inputFirst.substring(1))) {
             System.out.println("Change the coordinate of the begin");
             check = false;
-        } else if (!verAddress.contains(coordinateEnd.substring(0, 1)) || !horAddress.contains(coordinateEnd.substring(1))) {
+        } else if (!verAddress.contains(inputSecond.substring(0, 1)) || !horAddress.contains(inputSecond.substring(1))) {
             System.out.println("Change the coordinate of the end");
             check = false;
         } else if (!checkReplacementOfShip(beginHor, beginVer, endHor, endVer, length)) {
@@ -90,6 +114,7 @@ public class Main {
             checkReplacement = false;
         } else {
             //check placing to another one;
+            search:
             for (int i = beginHor - 1; i <= endHor + 1; i++) {
                 if (i > 0 && i < 10) {
                     for (int j = beginVer - 1; j <= endVer + 1; j++) {
@@ -97,7 +122,7 @@ public class Main {
                             if (battleField[j][i] == 'O') {
                                 System.out.println("Error! You placed it too close to another one. Try again:");
                                 checkReplacement = false;
-                                break;
+                                break search;
                             }
                         }
                     }
